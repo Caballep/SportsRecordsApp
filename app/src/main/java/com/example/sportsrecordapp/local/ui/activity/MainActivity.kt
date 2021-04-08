6b6a40bded07e42020fa73c3a1fcd9241c4fbcf6
Caballep <angel.caballep@gmail.com>
2021-04-08 07:26:56 -0500

    Structuring project

--- app/src/main/java/com/example/sportsrecordapp/local/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/sportsrecordapp/local/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.sportsrecordapp
+package com.example.sportsrecordapp.local.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sportsrecordapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
